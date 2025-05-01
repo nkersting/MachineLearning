2e6e224e39084e708a170d4138be6f2094b4e188
Lord Nicholas Stephen Kersting <lordkersting@Lords-MacBook-Air.attlocal.net>
2025-04-30 21:22:30 -0500

    initial

---
+++ higgs/AMS.py
@@ -0,0 +1,14 @@
+def AMS(s, b):
+    """ Approximate Median Significance defined as:
+        AMS = sqrt(
+                2 { (s + b + b_r) log[1 + (s/(b+b_r))] - s}
+              )        
+    where b_r = 10, b = background, s = signal, log is natural logarithm """
+    
+    br = 10.0
+    radicand = 2 *( (s+b+br) * math.log (1.0 + s/(b+br)) -s)
+    if radicand < 0:
+        print 'radicand is negative. Exiting'
+        exit()
+    else:
+        return math.sqrt(radicand)
